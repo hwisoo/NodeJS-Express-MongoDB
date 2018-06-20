6692727e1959ae98e21d2952037cf8b63ac7e0d4
James Cho <hwisoocho@gmail.com>
2018-06-19 20:23:41 -0700

    passport facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '373875259800684',
+        clientSecret: 'bcb5c3e952a5f0ca3958585da519f350'
+    }
 }
